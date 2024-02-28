bd0d2c01fb726abfb35f9de57d1f1885db957906
Caoimhín Arnott <118535981+Kwee-Veen@users.noreply.github.com>
2024-02-28 14:22:07 +0000

    v2.0: RDS DB creation, error handling, monitoring
    Script now generates an AWS RDS instance running MySQL Community Edition.
    Code robustness enhanced by wrapping functions in try / error blocks.
    Monitoring.sh file altered to also display additional metadata.

--- devops_1.py
+++ devops_1.py
@@ -5,50 +5,58 @@ import boto3, webbrowser, time, json, requests, uuid, subprocess
 ec2 = boto3.resource('ec2')
 
 # Creating ec2 instance with UserData script to initiate apache server, displaying current instance metadata
-new_instances = ec2.create_instances(
-  ImageId = 'ami-0440d3b780d96b29d',
-  MinCount = 1,
-  MaxCount = 1,
-  InstanceType = 't2.nano',
-  SecurityGroups = [
-    'launch-wizard-1'
-  ],
-  TagSpecifications = [
-    {
-      'ResourceType': 'instance',
-      'Tags': [
-        {
-          'Key': 'Name',
-          'Value': 'Script-generated web server'
-        },
-      ]
-    },
-  ],
-  KeyName = 'carnottKey',
-  UserData="""#!/bin/bash
-  yum update -y
-  yum install httpd -y
-  systemctl enable httpd
-  systemctl start httpd
-  TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-  echo "This instance is running in availability zone:" > index.html
-  curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone >> index.html
-  echo "<hr>The instance ID is: " >> index.html
-  curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id >> index.html
-  echo "<hr>The instance type is: " >> index.html
-  curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-type >> index.html
-  echo "<hr>The instance AMI ID is: " >> index.html
-  curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/ami-id >> index.html
-  echo "<hr>The instance's public IPv4 address is: " >> index.html
-  curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4 >> index.html
-  echo "<hr><br>Want a useless fact?" >> index.html
-  echo "<br>Course you do!" >> index.html
-  echo "<br><br>Q: How many satellites are in space?" >> index.html
-  echo "<br>A: 2,271. Russia has the most in orbit at 1,324 satellites, followed by the U.S. with 658. This does not count orbiting debris, just spacecraft." >> index.html
-  sudo mv index.html /var/www/html/index.html >> index.html
-  """
-)
-print (new_instances[0].id)
+
+try:
+  new_instances = ec2.create_instances(
+    ImageId = 'ami-0440d3b780d96b29d',
+    MinCount = 1,
+    MaxCount = 1,
+    InstanceType = 't2.nano',
+    SecurityGroups = [
+      'launch-wizard-1'
+    ],
+    TagSpecifications = [
+      {
+        'ResourceType': 'instance',
+        'Tags': [
+          {
+            'Key': 'Name',
+            'Value': 'Script-generated web server'
+          },
+        ]
+      },
+    ],
+    KeyName = 'carnottKey',
+    UserData="""#!/bin/bash
+    yum update -y
+    yum install httpd -y
+    systemctl enable httpd
+    systemctl start httpd
+    TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+    echo "This instance is running in availability zone:" > index.html
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone >> index.html
+    echo "<hr>The instance ID is: " >> index.html
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id >> index.html
+    echo "<hr>The instance type is: " >> index.html
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-type >> index.html
+    echo "<hr>The instance AMI ID is: " >> index.html
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/ami-id >> index.html
+    echo "<hr>The instance's public IPv4 address is: " >> index.html
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4 >> index.html
+    echo "<hr><br>Want a useless fact?" >> index.html
+    echo "<br>Course you do!" >> index.html
+    echo "<br><br>Q: How many satellites are in space?" >> index.html
+    echo "<br>A: 2,271. Russia has the most in orbit at 1,324 satellites, followed by the U.S. with 658. This does not count orbiting debris, just spacecraft." >> index.html
+    sudo mv index.html /var/www/html/index.html >> index.html
+    curl https://s3.us-east-1a.amazonaws.com/aws-xray-assets.us-east-1a/xray-daemon/aws-xray-daemon-3.x.rpm -o /home/ec2-user/xray.rpm
+    yum install -y /home/ec2-user/xray.rpm
+    """
+  )
+  print (new_instances[0].id)
+except Exception as error:
+    print (error)
+    print ("Could not create an ec2 instance. Exiting Script - please investigate the source of the error")
+    quit()
 
 # Creating s3 bucket
 s3 = boto3.resource("s3")
@@ -61,6 +69,8 @@ try:
     print (response)
 except Exception as error:
     print (error)
+    print ("Could not create an s3 bucket. Exiting script - please investigate the source of the error.")
+    quit()
 
 # Saving logo.jpg locally
 img_url = 'http://devops.witdemo.net/logo.jpg'
@@ -96,36 +106,73 @@ website_configuration = {
     'IndexDocument': {'Suffix': 'index.html'},
 }
 bucket_website = s3.BucketWebsite(f'{bucketName}')
-response = bucket_website.put(WebsiteConfiguration=website_configuration)
+try:
+  response = bucket_website.put(WebsiteConfiguration=website_configuration)
+except Exception as error:
+    print (error)
 
 # Deleting s3 bucket's public access block, creating & uploading a new policy permitting public traffic
 s3client = boto3.client("s3")
-s3client.delete_public_access_block(Bucket=(f'{bucketName}'))
-bucket_policy = {
-    "Version": "2012-10-17",
-    "Statement": [
-    {
-        "Sid": "PublicReadGetObject",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": ["s3:GetObject"],
-        "Resource": f"arn:aws:s3:::{bucketName}/*"
-    }
-    ]
-}
-s3.Bucket(f'{bucketName}').Policy().put(Policy=json.dumps(bucket_policy))
-print ("s3 Policy amended")
-time.sleep(1)
+try:
+  s3client.delete_public_access_block(Bucket=(f'{bucketName}'))
+  bucket_policy = {
+      "Version": "2012-10-17",
+      "Statement": [
+      {
+          "Sid": "PublicReadGetObject",
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": ["s3:GetObject"],
+          "Resource": f"arn:aws:s3:::{bucketName}/*"
+      }
+      ]
+  }
+  s3.Bucket(f'{bucketName}').Policy().put(Policy=json.dumps(bucket_policy))
+  print ("s3 Policy amended")
+  time.sleep(1)
+except Exception as error:
+    print (error)
+    print ("s3 public access policy amendment unsuccessful")
 
 # Opening s3 bucket endpoint in browser
 print (f'Opening {bucketName} endpoint in browser')
 s3_endpoint = f'http://{bucketName}.s3-website-us-east-1.amazonaws.com'
-webbrowser.open_new_tab(s3_endpoint)
+try:
+  webbrowser.open_new_tab(s3_endpoint)
+except Exception as error:
+    print (error)
+    print ("Error: s3 bucket endpoint unopenable in browser")
 
 # Saving s3 bucket link to a local file
 print (f'Saving {s3_endpoint} link to local file "carnott-websites.txt"')
 print ('Generated s3 bucket link:', file=open('carnott-websites.txt', 'w'))
 print (s3_endpoint, file=open('carnott-websites.txt', 'a'))
+
+# Creating a MySQL Community RDS instance.
+# Note: RDS will be ready to connect to the generated ec2 instance via the web console,
+# but it will not be not automatically connected.
+rds = boto3.client('rds')
+rds_db_name = f'{bucketName}_rds'
+try:
+  rds.create_db_instance(
+    AllocatedStorage=10,
+    DBName=f'carnott_{randomSixCharacters}_rds',
+    AvailabilityZone="us-east-1a",
+    DBInstanceIdentifier=f'carnott-{randomSixCharacters}-rds',
+    VpcSecurityGroupIds=[
+      'sg-06400c24fd8b54b5a'
+    ],
+    DBInstanceClass="db.t2.micro",
+    Engine="mysql",
+    MasterUsername="root",
+    MasterUserPassword="secretss",
+    MaxAllocatedStorage=10,
+    Port=3306,
+  )
+  print ("RDS created; to connect it to the generated ec2 instance, please use the RDS web console")
+except Exception as error:
+  print (error)
+  print ("Error during creation of RDS instance, please see above")
 
 # Waiting until ec2 instance is running, then saving the IPv4 address as inst0_ip
 print ("Waiting until ec2 instance is running")
@@ -148,19 +195,35 @@ time.sleep(35)
 print (f'Copying monitoring.sh to {inst0_ip}')
 ec2_ipv4 = f'ec2-user@{inst0_ip}'
 scp_cmd = f'scp -o StrictHostKeyChecking=no -i carnottKey.pem monitoring.sh {ec2_ipv4}:.'
-subprocess.call(scp_cmd.split())
+try:
+  subprocess.call(scp_cmd.split())
+except Exception as error:
+    print (error)
+    print ("Could not successfully copy monitoring.sh to {ec2_ipv4}")
 
 # Modify monitoring.sh in the ec2 instance via ssh remote command execution
 print (f'Changing monitoring.sh permissions in {inst0_ip}')
 ssh_permissions_command = "chmod 700 monitoring.sh"
 ssh_permissions_full = f'ssh -i carnottKey.pem {ec2_ipv4} {ssh_permissions_command}'
-subprocess.call(ssh_permissions_full.split())
+try:
+  subprocess.call(ssh_permissions_full.split())
+except Exception as error:
+    print (error)
+    print ("Could not modify access to monitoring.sh script in {ec2_ipv4}")
 
 # Execute monitoring.sh in the ec2 instance via ssh remote command execution
 print (f'Executing monitoring.sh in {inst0_ip}:')
 ssh_execute = f'ssh -i carnottKey.pem {ec2_ipv4} "./monitoring.sh"'
-subprocess.call(ssh_execute.split())
+try:
+  subprocess.call(ssh_execute.split())
+except Exception as error:
+    print (error)
+    print ("Could not execute monitoring.sh in {ec2_ipv4}")
 
 # Opening ec2 instance link in browser
 print ("Launching ec2 static website in browser")
-webbrowser.open_new_tab(apache_url)
+try:
+  webbrowser.open_new_tab(apache_url)
+except Exception as error:
+    print (error)
+    print ("Could not launch link to {apache_url} in browser")
--- monitoring.sh
+++ monitoring.sh
@@ -2,13 +2,17 @@
 #
 # Some basic monitoring functionality; Tested on Amazon Linux 2023.
 #
-TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+PUBLIC_IPV4=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+PUBLIC_KEYS=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-keys/)
 MEMORYUSAGE=$(free -m | awk 'NR==2{printf "%.2f%%", $3*100/$2 }')
 PROCESSES=$(expr $(ps -A | grep -c .) - 1)
 HTTPD_PROCESSES=$(ps -A | grep -c httpd)
 
 echo "Instance ID: $INSTANCE_ID"
+echo "Public IPv4 address: $PUBLIC_IPV4"
+echo "Public keys: $PUBLIC_KEYS"
 echo "Memory utilisation: $MEMORYUSAGE"
 echo "No of processes: $PROCESSES"
 if [ $HTTPD_PROCESSES -ge 1 ]
